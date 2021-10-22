4ea687f4c7214beb9c6c7b57fa481e8cb433806b
AkshayDalvi15 <88661890+AkshayDalvi15@users.noreply.github.com>
2021-10-22 12:46:15 +0530

    added module file as per dependencies

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  Hello There
+  
+  
+</body>
+</html>
+
+
+
+
+
+<!-- <?php 
+// require './vendor/autoload.php';
+// use Symfony\Component\Yaml\Yaml;
+// // use vendor\symfony\Component\Yaml\Parser;
+// // use vendor\symfony\Component\Yaml\Dumper;
+// $value = Yaml::parseFile('file1.yml');
+// // var_dump($value);
+// print_r($value);
+// //echo Yaml::dump($value,2,3, Yaml::DUMP_OBJECT);
+
+//?> 
+ -->
