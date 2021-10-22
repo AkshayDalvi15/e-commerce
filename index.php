3f5e471130b3402b5cb284a656691ed31c15de4b
AkshayDalvi15 <88661890+AkshayDalvi15@users.noreply.github.com>
2021-10-22 23:10:35 +0530

    Add new images and scss files

--- index.php
+++ index.php
@@ -3,17 +3,44 @@
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+  <link rel="stylesheet" href="./static/css/style.css" />
+  <title>E-commerce | Products</title>
 </head>
 <body>
-  Hello There
-  
-  
+
 </body>
 </html>
-
-
+  <nav>
+    <div class="heading"> 
+      <h2>Products</h2>
+        <span>Ecommerce / Products</span>
+    </div>
+    <div>
+      <button class="import"><i class="bi bi-download"></i> Import</button><button class="filter"><i
+              class="bi bi-funnel"></i> Filter</button><button class="download-report">
+          <i class="bi bi-cloud-download"> </i> Download Report
+      </button>
+    </div>
+  </nav>
+  <div class="card-container">
+        <div class="card">
+          <img src="./static/image/boy-tshirt.jpg" alt="" />
+            <p><?php echo "Boy  T-Shirt" ?></p>
+              <p><?php echo "$30" ?></p>
+        </div>
+        <div class="card">
+          <img src="./static/image/girl-dress.jpg" alt="" />
+            <p><?php echo "Girl Dress" ?></p>
+              <p><?php echo "$50" ?></p>
+        </div>
+        <div class="card">
+          <img src="./static/image/boy-tshirt.jpg" alt="" />
+            <p><?php echo "Boy  T-Shirt" ?></p>
+              <p><?php echo "$30" ?></p>
+        </div>
+  </div>
 
 
 
