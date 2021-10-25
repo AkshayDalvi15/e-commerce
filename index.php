69461fd390cc16e67ff464feb4cf8f2fccd43d10
AkshayDalvi15 <88661890+AkshayDalvi15@users.noreply.github.com>
2021-10-25 23:32:57 +0530

    Added the Search bar

--- index.php
+++ index.php
@@ -42,6 +42,14 @@
         </div>
   </div>
 
+  <div class="right-menu">
+    <div class="search-box">
+      <input type="text" placeholder="Search ..." /> <button>Search</button>
+    </div>
+  </div>
+
+
+  
 
 
 <!-- <?php 
